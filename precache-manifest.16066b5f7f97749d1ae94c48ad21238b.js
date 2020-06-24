9f14ea75638b5aff8f71b3d2e3e104495c4648eb
relaxedcoder <devjit.menghani@gmail.com>
2020-06-25 01:29:09 +0530

    Updates

---
+++ precache-manifest.16066b5f7f97749d1ae94c48ad21238b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6563a8bd448ab82770407b6c512bd489",
+    "url": "/Facebook-Hackathon/index.html"
+  },
+  {
+    "revision": "7436f32732711c496048",
+    "url": "/Facebook-Hackathon/static/css/main.586bf723.chunk.css"
+  },
+  {
+    "revision": "f5598e0d1025643731a1",
+    "url": "/Facebook-Hackathon/static/js/2.abde8ae2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Facebook-Hackathon/static/js/2.abde8ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7436f32732711c496048",
+    "url": "/Facebook-Hackathon/static/js/main.3d9bd2c5.chunk.js"
+  },
+  {
+    "revision": "f542a15b149ac2f149c2",
+    "url": "/Facebook-Hackathon/static/js/runtime-main.d036cb09.js"
+  },
+  {
+    "revision": "dafb457d13e02e9365a0f9e4a787468b",
+    "url": "/Facebook-Hackathon/static/media/doctor.dafb457d.png"
+  },
+  {
+    "revision": "e6151e5f2ad2c992de6db821930cc52c",
+    "url": "/Facebook-Hackathon/static/media/logo.e6151e5f.png"
+  },
+  {
+    "revision": "e796beed27f0c724b268c4be5a65c359",
+    "url": "/Facebook-Hackathon/static/media/medicine-vertical.e796beed.jpg"
+  },
+  {
+    "revision": "ea0476438f4961fc33341252f4e9bc3c",
+    "url": "/Facebook-Hackathon/static/media/medicine.ea047643.jpg"
+  },
+  {
+    "revision": "b6e74e33bf76ad35214f18237ad4acfd",
+    "url": "/Facebook-Hackathon/static/media/messenger.b6e74e33.PNG"
+  },
+  {
+    "revision": "2e3548cb4c7ae0e0c7a14b8bdac7a2fa",
+    "url": "/Facebook-Hackathon/static/media/vertical-small.2e3548cb.jpg"
+  }
+]);
